61957bec23c46f72918d59e7c28cfb2f9bbc4048
Franciel Ferreira <franciel.ferreira357@gmail.com>
2024-03-07 10:03:14 -0300

    adapt controller with sequelize

--- src/controllers/eventos_controller.js
+++ src/controllers/eventos_controller.js
@@ -1,23 +1,7 @@
-const sequelize = require('../config/database');
 const Evento = require('../models/evento');
 
 module.exports = class HomeController {
   static async index(req, res, next) {
-    try {
-      await sequelize.authenticate();
-      console.log('deu bão');
-    } catch (error) {
-      console.error('Deu ruim patrão', error);
-    }
-
-    res
-      .status(200)
-      .send([
-        new Evento(),
-        new Evento(),
-        new Evento(),
-        new Evento(),
-        new Evento(),
-      ]);
+    res.status(200).send([]);
   }
 };
